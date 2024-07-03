95560b14ba2f15715a60a2a1e194b9ef53f860c3
taiyo914 <taiyosuzuki0914@icloud.com>
2024-07-03 09:39:41 +0900

    ヘッダーと掲示板のリストを作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
-  return (
-    <div className="App">
+  const [posts, setPosts] = useState([]);
 
-    </div>
+  useEffect(() => {
+
+    const fetchPosts = async () => {
+      const response = await fetch('https://railway.bulletinboard.techtrain.dev/threads?offset=1');
+      const data = await response.json();
+      setPosts(data)
+      console.log(data);
+    };
+
+    fetchPosts();
+  },[]);
+
+  return (
+    <>
+      <Header/>
+      <ul className = "listBox">
+        {posts.map(post => (
+          <li key={post.id} className ="listItem">
+              {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
